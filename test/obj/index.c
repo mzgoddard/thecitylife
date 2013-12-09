cb300fcbf1fee7c95a947b63ab23ec6d06fda96e
Michael "Z" Goddard <mzgoddard@gmail.com>
2013-12-08 21:55:39 -0500

    Compare AQNumbers

--- test/obj/index.c
+++ test/obj/index.c
@@ -4,6 +4,7 @@ extern void suite_obj_obj();
 extern void suite_obj_list();
 extern void suite_obj_dictmap();
 extern void suite_obj_string();
+extern void suite_obj_number();
 extern void suite_obj_interfaceptr();
 
 void suite_obj() {
@@ -12,5 +13,6 @@ void suite_obj() {
   suite_obj_list();
   suite_obj_dictmap();
   suite_obj_string();
+  suite_obj_number();
   suite_obj_interfaceptr();
 }
